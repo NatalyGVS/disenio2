3270c5fbf3045d733e65c3b2cf0d68a473c3dc3d
NatalyGVS <nataly.vasquez9718@gmail.com>
2019-06-30 13:07:26 -0500

    proveedor v1.2 xd

--- application/views/proveedores/index.php
+++ application/views/proveedores/index.php
@@ -36,7 +36,7 @@
         <?php endif; ?>
 
         <!-- <?php //if(in_array('createMesas', $user_permission)): ?> -->
-          <button class="btn btn-primary" data-toggle="modal" data-target="#addModal">Añadir Proveedor</button> 
+          <button class="btn btn-primary" data-toggle="modal" data-target="#addModal">Añadir Proveedor</button>
           <br /> <br />
         <!-- ?php endif; ?> -->
 
@@ -54,7 +54,7 @@
                 <th>Direccion</th>
                 <th>Tipo de Proveedor</th>
                 <!--<?php //if(in_array('updateMesas', $user_permission) || in_array('deleteMesas', $user_permission)): ?>-->
-                  <th>Accion</th>
+                  <th>Action</th>
                 <!--<?php //endif; ?>-->
               </tr>
               </thead>
@@ -85,7 +85,7 @@
         <h4 class="modal-title">Añadir Proveedor</h4>
       </div>
 
-      < <form role="form" action="<?php echo base_url('proveedores/create') ?>" method="post" id="createForm">
+      <form role="form" action="<?php echo base_url('proveedores/create') ?>" method="post" id="createForm">
 
         <div class="modal-body">
 
@@ -120,7 +120,7 @@
           <button type="submit" class="btn btn-primary">Guardar cambios</button>
         </div>
 
-      </form> 
+      </form>
 
 
     </div><!-- /.modal-content -->
@@ -128,14 +128,89 @@
 </div><!-- /.modal -->
 <!--<?php //endif; ?>-->
 
+<!--<?php //if(in_array('updateMesas', $user_permission)): ?>-->
+<!-- edit brand modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="editModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Editar Proveedor</h4>
+      </div>
 
+      <form role="form" action="<?php echo base_url('proveedores/update') ?>" method="post" id="updateForm">
+
+        <div class="modal-body">
+          <div id="messages"></div>
+
+          <div class="form-group">
+            <label for="edit_brand_name">Razon Social</label>
+            <input type="text" class="form-control" id="edit_proveedores_razon_social" name="edit_proveedores_razon_social" placeholder="Ingresa Razon Social" autocomplete="off">
+          </div>
+
+          <div class="form-group">
+            <label for="edit_brand_name">RUCa</label>
+            <input type="text" class="form-control" id="edit_proveedores_RUC" name="edit_proveedores_RUC" placeholder="Ingresa RUC" autocomplete="off">
+          </div>
+
+          <div class="form-group">
+            <label for="edit_brand_name">Direccion</label>
+            <input type="text" class="form-control" id="edit_proveedores_direccion" name="edit_proveedores_direccion" placeholder="Ingresa Direccion" autocomplete="off">
+          </div>
+          
+          <div class="form-group">
+            <label for="edit_tipo_proveedor">Tipo Proveedor</label>
+            <select class="form-control" id="edit_tipo_proveedor" name="edit_tipo_proveedor">
+            <option value="1">tipo1</option>
+              <option value="2">tipo2</option>
+            </select>
+          </div>
+
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+          <button type="submit" class="btn btn-primary">Guardar cambios</button>
+        </div>
+
+      </form>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!--<?php //endif; ?>-->
+
+<!--<?php //if(in_array('deleteMesas', $user_permission)): ?>-->
+<!-- remove brand modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="removeModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Eliminar Proveedor</h4>
+      </div>
+
+      <form role="form" action="<?php echo base_url('proveedores/remove') ?>" method="post" id="removeForm">
+        <div class="modal-body">
+          <p>¿Realmente quieres eliminar esta proveedor?  Se deben eliminar con todossus productos</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+          <button type="submit" class="btn btn-primary">Guardar cambios</button>
+        </div>
+      </form>
+
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- <?php //endif; ?> -->
 
 
 <script type="text/javascript">
 var manageTable;
 
 $(document).ready(function() {
-   $("#proveedoresNav").addClass('active');
+  $("#proveedoresNav").addClass('active');
   
   // initialize the datatable  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
   manageTable = $('#manageTable').DataTable({
@@ -204,6 +279,122 @@ $(document).ready(function() {
   });
 
 });
+
+// edit function
+function editFunc(id)
+{ 
+  $.ajax({
+    url: 'fetchProveedoresDataById/'+id,
+    type: 'post',
+    dataType: 'json',
+    success:function(response) {
+
+      // $("#edit_mesas_name").val(response.name);
+      
+      
+
+      // submit the edit from 
+      $("#updateForm").unbind('submit').bind('submit', function() {
+        var form = $(this);
+
+        // remove the text-danger
+        $(".text-danger").remove();
+
+        $.ajax({
+          url: form.attr('action') + '/' + id,
+          type: form.attr('method'),
+          data: form.serialize(), // /converting the form data into array and sending it to server
+          dataType: 'json',
+          success:function(response) {
+
+            manageTable.ajax.reload(null, false); 
+
+            if(response.success === true) {
+              $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+                '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+              '</div>');
+
+
+              // hide the modal
+              $("#editModal").modal('hide');
+              // reset the form 
+              $("#updateForm .form-group").removeClass('has-error').removeClass('has-success');
+
+            } else {
+
+              if(response.messages instanceof Object) {
+                $.each(response.messages, function(index, value) {
+                  var id = $("#"+index);
+
+                  id.closest('.form-group')
+                  .removeClass('has-error')
+                  .removeClass('has-success')
+                  .addClass(value.length > 0 ? 'has-error' : 'has-success');
+                  
+                  id.after(value);
+
+                });
+              } else {
+                $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+                  '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                  '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+                '</div>');
+              }
+            }
+          }
+        }); 
+
+        return false;
+      });
+
+    }
+  });
+}
+
+// remove functions 
+function removeFunc(id)
+{
+  if(id) {
+    $("#removeForm").on('submit', function() {
+
+      var form = $(this);
+
+      // remove the text-danger
+      $(".text-danger").remove();
+
+      $.ajax({
+        url: form.attr('action'),
+        type: form.attr('method'),
+        data: { proveedores_id:id }, 
+        dataType: 'json',
+        success:function(response) {
+
+          manageTable.ajax.reload(null, false); 
+
+          if(response.success === true) {
+            $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+              '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+              '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+            '</div>');
+
+            // hide the modal
+            $("#removeModal").modal('hide');
+
+          } else {
+
+            $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+              '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+              '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+            '</div>'); 
+          }
+        }
+      }); 
+
+      return false;
+    });
+  }
+}
 
 
 </script>
